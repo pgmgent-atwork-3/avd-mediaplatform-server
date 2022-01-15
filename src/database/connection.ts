606bacb26e54e46a1815be9b67d42f9b8550d6a3
Dylan Cathelijn <dylan_cathelyn@hotmail.be>
2022-01-15 16:42:34 +0100

    Add Database URL in .env, fix Procfile run command

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,17 +2,27 @@ import { ConnectionOptions } from 'typeorm';
 
 import * as dotenv from 'dotenv';
 
-const typeOrmConfig: ConnectionOptions = {
-  type: 'postgres',
-  host: process.env.PG_HOST,
-  port: Number(process.env.PG_PORT),
-  username: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
-  logging: true,
-};
+const typeOrmConfig: ConnectionOptions = !process.env.DATABASE_URL
+  ? {
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+    }
+  : {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: { rejectUnauthorized: false },
+    };
+
+console.log(typeOrmConfig);
 
 module.exports = {
   ...typeOrmConfig,
