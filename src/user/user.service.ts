9ae281c809153342e86cd5adc4c4406cf06df5c9
Dylan Cathelijn <dylan_cathelyn@hotmail.be>
2022-01-18 21:28:09 +0100

    Fixes #26 - User

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -11,30 +11,30 @@ export class UserService {
     @InjectRepository(User) readonly userRepository: Repository<User>,
   ) {}
 
-  async create(createUserInput: CreateUserInput) {
+  async create(createUserInput: CreateUserInput): Promise<User> {
     const user = this.userRepository.create(createUserInput);
     return await this.userRepository.save(user);
   }
 
-  async findAll() {
+  async findAll(): Promise<User[]> {
     return await this.userRepository.find();
   }
 
-  async findOne(id: number) {
+  async findOne(id: number): Promise<User> {
     return await this.userRepository.findOne(id);
   }
 
-  async findByUsername(username: string) {
+  async findByUsername(username: string): Promise<User> {
     return await this.userRepository.findOne({ username });
   }
 
-  async update(id: number, updateUserInput: UpdateUserInput) {
+  async update(id: number, updateUserInput: UpdateUserInput): Promise<User> {
     const user = await this.userRepository.findOne(id);
     this.userRepository.merge(user, updateUserInput);
     return await this.userRepository.save(user);
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<User> {
     const user = await this.userRepository.findOne(id);
     await this.userRepository.remove(user);
     return user;
