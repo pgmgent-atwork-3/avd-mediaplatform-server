9611fd7e1a6b2dcf25d861a7426ba56d454d6552
Dylan Cathelijn <dylan_cathelyn@hotmail.be>
2022-01-18 15:04:56 +0100

    Fixes #20

--- src/database/factories/live-history.factory.ts
+++ src/database/factories/live-history.factory.ts
@@ -1,17 +1,16 @@
 import { LiveHistory } from 'src/live-history/entities/live-history.entity';
 import { User } from 'src/user/entities/user.entity';
 import { define, factory } from 'typeorm-seeding';
+import { makeid } from './video.factory';
 
 define(LiveHistory, (faker: any) => {
   const liveHistory = new LiveHistory();
-  liveHistory.title = faker.fake("{{name.lastName}} {{name.firstName}}'s livestream nr.{{faker.random.number(55)}} ")
 
-  liveHistory.url = faker.internet.url();;
-  liveHistory.thumbnail = faker.image.image();
-  liveHistory.user = factory(User)() as any;;
-  liveHistory.streamed_on = faker.date.past(); 
-
-  
+  liveHistory.title = faker.lorem.sentence();
+  liveHistory.url = `https://youtube.com/${makeid(15)}`;
+  liveHistory.user = factory(User)() as any;
+  liveHistory.thumbnail = 'https://lorempicsum.com/futurama/200/200/1';
+  liveHistory.streamed_on = new Date();
 
   return liveHistory;
 });
