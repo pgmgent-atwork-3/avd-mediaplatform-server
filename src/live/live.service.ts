4e1cab1a6cd969c187b9ec4c18060a2e55fa75bf
Dylan Cathelijn <dylan_cathelyn@hotmail.be>
2022-01-18 21:14:21 +0100

    Fixes #26 - Live

--- src/live/live.service.ts
+++ src/live/live.service.ts
@@ -12,25 +12,25 @@ export class LiveService {
     private readonly liveRepository: Repository<Live>,
   ) {}
 
-  async create(createLiveInput: CreateLiveInput) {
+  async create(createLiveInput: CreateLiveInput): Promise<Live> {
     const newLive = this.liveRepository.create(createLiveInput);
     return this.liveRepository.save(newLive);
   }
 
-  async findAll() {
+  async findAll(): Promise<Live[]> {
     return await this.liveRepository.find({ relations: ['tags'] });
   }
 
-  async findOne(id: number) {
+  async findOne(id: number): Promise<Live> {
     return await this.liveRepository.findOne(id);
   }
 
-  async update(id: number, updateLiveInput: UpdateLiveInput) {
+  async update(id: number, updateLiveInput: UpdateLiveInput): Promise<Live> {
     await this.liveRepository.update(id, updateLiveInput);
     return await this.liveRepository.findOne(id);
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<Live> {
     const live = await this.liveRepository.findOne(id);
     await this.liveRepository.remove(live);
     return live;
