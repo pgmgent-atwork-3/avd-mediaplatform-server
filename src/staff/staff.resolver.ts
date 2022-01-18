8356f41bdf1c119676e01676a514a2e5d1ab2327
Dylan Cathelijn <dylan_cathelyn@hotmail.be>
2022-01-18 21:18:40 +0100

    Fixes #26 - Staff

--- src/staff/staff.resolver.ts
+++ src/staff/staff.resolver.ts
@@ -5,7 +5,7 @@ import { CreateStaffInput } from './dto/create-staff.input';
 import { UpdateStaffInput } from './dto/update-staff.input';
 import Role from 'src/user/enums/role.enum';
 import RoleGuard from 'src/auth/role.guard';
-import { UseGuards } from '@nestjs/common';
+import { ParseIntPipe, UseGuards } from '@nestjs/common';
 
 @Resolver(() => Staff)
 export class StaffResolver {
@@ -13,31 +13,39 @@ export class StaffResolver {
 
   @Mutation(() => Staff)
   @UseGuards(RoleGuard(Role.Admin))
-  createStaff(@Args('createStaffInput') createStaffInput: CreateStaffInput) {
+  createStaff(
+    @Args('createStaffInput') createStaffInput: CreateStaffInput,
+  ): Promise<Staff> {
     return this.staffService.create(createStaffInput);
   }
 
   @Query(() => [Staff], { name: 'staffs' })
   @UseGuards(RoleGuard(Role.Admin))
-  findAll() {
+  findAll(): Promise<Staff[]> {
     return this.staffService.findAll();
   }
 
   @Query(() => Staff, { name: 'staff' })
   @UseGuards(RoleGuard(Role.Admin))
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  findOne(
+    @Args('id', { type: () => Int }, ParseIntPipe) id: number,
+  ): Promise<Staff> {
     return this.staffService.findOne(id);
   }
 
   @Mutation(() => Staff)
   @UseGuards(RoleGuard(Role.Admin))
-  updateStaff(@Args('updateStaffInput') updateStaffInput: UpdateStaffInput) {
+  updateStaff(
+    @Args('updateStaffInput') updateStaffInput: UpdateStaffInput,
+  ): Promise<Staff> {
     return this.staffService.update(updateStaffInput.id, updateStaffInput);
   }
 
   @Mutation(() => Staff)
   @UseGuards(RoleGuard(Role.Admin))
-  removeStaff(@Args('id', { type: () => Int }) id: number) {
+  removeStaff(
+    @Args('id', { type: () => Int }, ParseIntPipe) id: number,
+  ): Promise<Staff> {
     return this.staffService.remove(id);
   }
 }
